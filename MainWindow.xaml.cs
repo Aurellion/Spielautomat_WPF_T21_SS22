e90f7975907cfa3340f83837e61a4a61e974cdfe
Johannes B <johannes.bock@live.de>
2022-03-15 13:14:30 +0100

    15.03.22_1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace Spielautomat
     /// </summary>
     public partial class MainWindow : Window
     {
+        int guthaben = 5;
         public MainWindow()
         {
             InitializeComponent();
+            
+            TB_Guthaben.Text = guthaben.ToString();
         }
 
         //private void button_Message_Click(object sender, RoutedEventArgs e)
@@ -34,7 +37,46 @@ namespace Spielautomat
 
         private void B_Spiel_Click(object sender, RoutedEventArgs e)
         {
-            TB_Zahl1.Text = "10";
+            Random rnd = new Random();
+            int einsatz, gewinn;
+            einsatz = Convert.ToInt32(TBox_Einsatz.Text);
+            int z1, z2, z3;
+
+            if (einsatz <= 0) MessageBox.Show("Der Einsatz muss größer als Null sein!", "Falscher Einsatz");
+            else if (einsatz > guthaben) MessageBox.Show("Nicht genug Guthaben.","Falscher Einsatz");
+            else
+            {
+                guthaben -= einsatz;
+                TB_Guthaben.Text = guthaben.ToString();
+                z1 = rnd.Next(0, 10);
+                z2 = rnd.Next(0, 10);
+                z3 = rnd.Next(0, 10);
+                TB_Zahl1.Text = z1.ToString();
+                TB_Zahl2.Text = z2.ToString();
+                TB_Zahl3.Text = z3.ToString();
+
+            }
+                   
+
+            if (guthaben == 0)
+            {
+                MessageBoxResult ergebnis;
+                string messageboxtext = "Noch eine Runde?";
+                string messageboxcaption = "Spiel Verloren";
+
+                ergebnis = MessageBox.Show(messageboxtext, messageboxcaption, MessageBoxButton.YesNo);
+
+                switch (ergebnis)
+                {
+                    case MessageBoxResult.Yes:
+                        guthaben = 5;
+                        //Textbox anpassen
+                        break;
+                    case MessageBoxResult.No:
+                        Application.Current.Shutdown();
+                        break;
+                }
+            }
         }
     }
 }
