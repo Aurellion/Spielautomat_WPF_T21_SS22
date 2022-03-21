67bfaccfd18875f8be7d638ca89bfa8da5fa5ca3
Johannes B <johannes.bock@live.de>
2022-03-21 14:58:10 +0100

    eingabeprüfung mit regex

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Spielautomat
 {
@@ -37,26 +38,50 @@ namespace Spielautomat
 
         private void B_Spiel_Click(object sender, RoutedEventArgs e)
         {
+            Spielen();          
+        }
+
+        private void Spielen()
+        {
             Random rnd = new Random();
             int einsatz, gewinn;
             einsatz = Convert.ToInt32(TBox_Einsatz.Text);
             int z1, z2, z3;
 
             if (einsatz <= 0) MessageBox.Show("Der Einsatz muss größer als Null sein!", "Falscher Einsatz");
-            else if (einsatz > guthaben) MessageBox.Show("Nicht genug Guthaben.","Falscher Einsatz");
+            else if (einsatz > guthaben) MessageBox.Show("Nicht genug Guthaben.", "Falscher Einsatz");
             else
             {
                 guthaben -= einsatz;
                 TB_Guthaben.Text = guthaben.ToString();
-                z1 = rnd.Next(0, 10);
-                z2 = rnd.Next(0, 10);
-                z3 = rnd.Next(0, 10);
+                z1 = rnd.Next(1, 7);
+                z2 = rnd.Next(1, 7);
+                z3 = rnd.Next(1, 7);
                 TB_Zahl1.Text = z1.ToString();
                 TB_Zahl2.Text = z2.ToString();
                 TB_Zahl3.Text = z3.ToString();
 
+                if (z1 == z2 && z2 == z3)
+                {
+                    gewinn = einsatz * 3;
+                    MessageBox.Show("Der Gewinn beträgt: " + gewinn, "Gewonnen!");
+                    guthaben += gewinn;
+                    TB_Guthaben.Text = guthaben.ToString();
+                }
+                else if (z1 == z2 || z1 == z3 || z2 == z3)
+                {
+                    gewinn = einsatz + 3;
+                    MessageBox.Show("Der Gewinn beträgt: " + gewinn, "Gewonnen!");
+                    guthaben += gewinn;
+                    TB_Guthaben.Text = guthaben.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("leider kein Gewinn.", "Schade...");
+                }
+
             }
-                   
+
 
             if (guthaben == 0)
             {
@@ -70,13 +95,42 @@ namespace Spielautomat
                 {
                     case MessageBoxResult.Yes:
                         guthaben = 5;
-                        //Textbox anpassen
+                        TB_Guthaben.Text = "5";
+                        TB_Zahl1.Text = "0";
+                        TB_Zahl2.Text = "0";
+                        TB_Zahl3.Text = "0";
                         break;
                     case MessageBoxResult.No:
                         Application.Current.Shutdown();
                         break;
                 }
             }
+        }
+
+        private void ZahlenEingabeprüfung(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]");
+            e.Handled = regex.IsMatch(e.Text);
+
+            //Bedeutung
+            // ^ Beginn der Strings
+            // [-+]? Vorzeichen
+            // [0-9]* 0 oder mehr Ziffern
+            // [0-9]+ 1 oder mehr ziffern
+            // [a-z] alle Kleinbuchstaben
+            // [A-Z] alle Großbuchstaben
+            // [a-zA-Z] alle Buchstaben
+            // . jedes beliebige Zeichen
+            // \. Dezimalpunkt
+            // [.,] Komma oder Punkt
+            // $ Ende des Strings
+
+            //Bsp ^[+-]?[0-9]*[.,]?[0-9]+$
+        }
+
+        private void TBox_Einsatz_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) Spielen();
         }
     }
 }
